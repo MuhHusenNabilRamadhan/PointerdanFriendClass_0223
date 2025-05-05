d5304c80e405ccb03c8fba38b6cf63e65415bee4
MuhhusenNabilRamadhan <secondchoice230906@gmail.com>
2025-05-05 19:00:21 +0700

    pointer reference dan dereference

--- pointerSatu@.cpp
+++ pointerSatu@.cpp
@@ -9,5 +9,21 @@ public :
     {
         cout << "No Induk = " << nim <<endl;
     }
+};
+
+int main() 
+{
+    mahasiswa mhs; //object mhs
+    mhs.nim = 2024;
+    mhs.showNim(); //akses member
+
+    mahasiswa &refMhs = mhs; //pointer reference refMhs
+    refMhs.nim = 2; //akses member
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointerr dereferece pMhs
+    pMhs->nim = 3; //arrow operator
+    pMhs->showNim();
+    return 0;  
 
 };
